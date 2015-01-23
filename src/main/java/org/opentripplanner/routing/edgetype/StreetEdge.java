77c8109e2e930bee86df99f287c4b7525893d346
Kathryn Killebrew <kkillebrew@azavea.com>
2015-01-23 17:58:15 -0500

    Refactor edge weighting logic into separate class.
    No functionality should be affected.

--- src/main/java/org/opentripplanner/routing/edgetype/StreetEdge.java
+++ src/main/java/org/opentripplanner/routing/edgetype/StreetEdge.java
@@ -30,8 +30,6 @@ import org.opentripplanner.routing.core.TraverseMode;
 import org.opentripplanner.routing.core.TraverseModeSet;
 import org.opentripplanner.routing.graph.Edge;
 import org.opentripplanner.routing.graph.Graph;
-import org.opentripplanner.routing.util.ElevationUtils;
-import org.opentripplanner.routing.vertextype.IntersectionVertex;
 import org.opentripplanner.routing.vertextype.StreetVertex;
 import org.opentripplanner.util.BitSetUtils;
 import org.slf4j.Logger;
@@ -61,8 +59,6 @@ public class StreetEdge extends Edge implements Cloneable {
     public static final int ANY_PLATFORM_MASK = 24;
     public static final int CROSSING_CLASS_MASK = 7; // ignore platform
     public static final int CLASS_LINK = 32; // on/offramps; OSM calls them "links"
-
-    private static final double GREENWAY_SAFETY_FACTOR = 0.1;
 
     // TODO(flamholz): do something smarter with the car speed here.
     public static final float DEFAULT_CAR_SPEED = 11.2f;
@@ -204,7 +200,7 @@ public class StreetEdge extends Edge implements Cloneable {
         return getPermission().allows(modes);
     }
 
-    private boolean canTraverse(RoutingRequest options, TraverseMode mode) {
+    public boolean canTraverse(RoutingRequest options, TraverseMode mode) {
         if (options.wheelchairAccessible) {
             if (!isWheelchairAccessible()) {
                 return false;
@@ -237,7 +233,7 @@ public class StreetEdge extends Edge implements Cloneable {
     public State traverse(State s0) {
         final RoutingRequest options = s0.getOptions();
         final TraverseMode currMode = s0.getNonTransitMode();
-        StateEditor editor = doTraverse(s0, options, s0.getNonTransitMode());
+        StateEditor editor = StreetEdgeTraversal.doTraverse(this, s0, options, s0.getNonTransitMode());
         State state = (editor == null) ? null : editor.makeState();
         /* Kiss and ride support. Mode transitions occur without the explicit loop edges used in park-and-ride. */
         if (options.kissAndRide) {
@@ -245,7 +241,7 @@ public class StreetEdge extends Edge implements Cloneable {
                 // Branch search to "unparked" CAR mode ASAP after transit has been used.
                 // Final WALK check prevents infinite recursion.
                 if (s0.isCarParked() && s0.isEverBoarded() && currMode == TraverseMode.WALK) {
-                    editor = doTraverse(s0, options, TraverseMode.CAR);
+                    editor = StreetEdgeTraversal.doTraverse(this, s0, options, TraverseMode.CAR);
                     if (editor != null) {
                         editor.setCarParked(false); // Also has the effect of switching to CAR
                         State forkState = editor.makeState();
@@ -259,7 +255,7 @@ public class StreetEdge extends Edge implements Cloneable {
                 // Irrevocable transition from driving to walking. "Parking" means being dropped off in this case.
                 // Final CAR check needed to prevent infinite recursion.
                 if ( ! s0.isCarParked() && ! getPermission().allows(TraverseMode.CAR) && currMode == TraverseMode.CAR) {
-                    editor = doTraverse(s0, options, TraverseMode.WALK);
+                    editor = StreetEdgeTraversal.doTraverse(this, s0, options, TraverseMode.WALK);
                     if (editor != null) {
                         editor.setCarParked(true); // has the effect of switching to WALK and preventing further car use
                         return editor.makeState(); // return only the "parked" walking state
@@ -269,262 +265,6 @@ public class StreetEdge extends Edge implements Cloneable {
             }
         }
         return state;
-    }
-
-    /** return a StateEditor rather than a State so that we can make parking/mode switch modifications for kiss-and-ride. */
-    private StateEditor doTraverse(State s0, RoutingRequest options, TraverseMode traverseMode) {
-        boolean walkingBike = options.walkingBike;
-        boolean backWalkingBike = s0.isBackWalkingBike();
-        TraverseMode backMode = s0.getBackMode();
-        Edge backEdge = s0.getBackEdge();
-        if (backEdge != null) {
-            // No illegal U-turns.
-            // NOTE(flamholz): we check both directions because both edges get a chance to decide
-            // if they are the reverse of the other. Also, because it doesn't matter which direction
-            // we are searching in - these traversals are always disallowed (they are U-turns in one direction
-            // or the other).
-            // TODO profiling indicates that this is a hot spot.
-            if (this.isReverseOf(backEdge) || backEdge.isReverseOf(this)) {
-                return null;
-            }
-        }
-
-        // Ensure we are actually walking, when walking a bike
-        backWalkingBike &= TraverseMode.WALK.equals(backMode);
-        walkingBike &= TraverseMode.WALK.equals(traverseMode);
-
-        /* Check whether this street allows the current mode. If not and we are biking, attempt to walk the bike. */
-        if (!canTraverse(options, traverseMode)) {
-            if (traverseMode == TraverseMode.BICYCLE) {
-                return doTraverse(s0, options.bikeWalkingOptions, TraverseMode.WALK);
-            }
-            return null;
-        }
-
-        // Automobiles have variable speeds depending on the edge type
-        double speed = calculateSpeed(options, traverseMode);
-
-        double time = getDistance() / speed;
-        double weight;
-        // TODO(flamholz): factor out this bike, wheelchair and walking specific logic to somewhere central.
-        if (options.wheelchairAccessible) {
-            weight = getSlopeSpeedEffectiveLength() / speed;
-        } else if (traverseMode.equals(TraverseMode.BICYCLE)) {
-            time = getSlopeSpeedEffectiveLength() / speed;
-            switch (options.optimize) {
-            case SAFE:
-                weight = bicycleSafetyFactor * getDistance() / speed;
-                break;
-            case GREENWAYS:
-                weight = bicycleSafetyFactor * getDistance() / speed;
-                if (bicycleSafetyFactor <= GREENWAY_SAFETY_FACTOR) {
-                    // greenways are treated as even safer than they really are
-                    weight *= 0.66;
-                }
-                break;
-            case FLAT:
-                /* see notes in StreetVertex on speed overhead */
-                weight = getDistance() / speed + getSlopeWorkCostEffectiveLength();
-                break;
-            case QUICK:
-                weight = getSlopeSpeedEffectiveLength() / speed;
-                break;
-            case TRIANGLE:
-                double quick = getSlopeSpeedEffectiveLength();
-                double safety = bicycleSafetyFactor * getDistance();
-                // TODO This computation is not coherent with the one for FLAT
-                double slope = getSlopeWorkCostEffectiveLength();
-                weight = quick * options.triangleTimeFactor + slope
-                        * options.triangleSlopeFactor + safety
-                        * options.triangleSafetyFactor;
-                weight /= speed;
-                break;
-            default:
-                weight = getDistance() / speed;
-            }
-        } else {
-            if (walkingBike) {
-                // take slopes into account when walking bikes
-                time = getSlopeSpeedEffectiveLength() / speed;
-            }
-            weight = time;
-            if (traverseMode.equals(TraverseMode.WALK)) {
-                // take slopes into account when walking
-                // FIXME: this causes steep stairs to be avoided. see #1297.
-                double costs = ElevationUtils.getWalkCostsForSlope(getDistance(), getMaxSlope());
-                // as the cost walkspeed is assumed to be for 4.8km/h (= 1.333 m/sec) we need to adjust
-                // for the walkspeed set by the user
-                double elevationUtilsSpeed = 4.0 / 3.0;
-                weight = costs * (elevationUtilsSpeed / speed);
-
-                time = weight; //treat cost as time, as in the current model it actually is the same (this can be checked for maxSlope == 0)
-                /*
-                // debug code
-                if(weight > 100){
-                    double timeflat = length / speed;
-                    System.out.format("line length: %.1f m, slope: %.3f ---> slope costs: %.1f , weight: %.1f , time (flat):  %.1f %n", length, elevationProfile.getMaxSlope(), costs, weight, timeflat);
-                }
-                */
-            }
-        }
-
-        if (options.preferBenches && (getBenchCount() > 0)) {
-            LOG.info("Found edge {}: {} has a bench!  Weighting it.", getName(), getId());
-            weight *= 0.001;
-            time *= 0.001;
-        }
-
-        if (options.preferToilets && (getToiletCount() > 0)) {
-            LOG.info("Found edge {}: {} has a toilet!  Weighting it.", getName(), getId());
-            weight *= 0.001;
-            time *= 0.001;
-        }
-
-        if (isStairs()) {
-            weight *= options.stairsReluctance;
-        } else {
-            // TODO: this is being applied even when biking or driving.
-            weight *= options.walkReluctance;
-        }
-
-        StateEditor s1 = s0.edit(this);
-        s1.setBackMode(traverseMode);
-        s1.setBackWalkingBike(walkingBike);
-
-        /* Compute turn cost. */
-        StreetEdge backPSE;
-        if (backEdge != null && backEdge instanceof StreetEdge) {
-            backPSE = (StreetEdge) backEdge;
-            RoutingRequest backOptions = backWalkingBike ?
-                    s0.getOptions().bikeWalkingOptions : s0.getOptions();
-            double backSpeed = backPSE.calculateSpeed(backOptions, backMode);
-            double realTurnCost;  // Units are seconds.
-
-            // Apply turn restrictions
-            if (options.arriveBy && !canTurnOnto(backPSE, s0, backMode)) {
-                return null;
-            } else if (!options.arriveBy && !backPSE.canTurnOnto(this, s0, traverseMode)) {
-                return null;
-            }
-
-            /*
-             * This is a subtle piece of code. Turn costs are evaluated differently during
-             * forward and reverse traversal. During forward traversal of an edge, the turn
-             * *into* that edge is used, while during reverse traversal, the turn *out of*
-             * the edge is used.
-             *
-             * However, over a set of edges, the turn costs must add up the same (for
-             * general correctness and specifically for reverse optimization). This means
-             * that during reverse traversal, we must also use the speed for the mode of
-             * the backEdge, rather than of the current edge.
-             */
-            if (options.arriveBy && tov instanceof IntersectionVertex) { // arrive-by search
-                IntersectionVertex traversedVertex = ((IntersectionVertex) tov);
-
-                realTurnCost = backOptions.getIntersectionTraversalCostModel().computeTraversalCost(
-                        traversedVertex, this, backPSE, backMode, backOptions, (float) speed,
-                        (float) backSpeed);
-            } else if (!options.arriveBy && fromv instanceof IntersectionVertex) { // depart-after search
-                IntersectionVertex traversedVertex = ((IntersectionVertex) fromv);
-
-                realTurnCost = options.getIntersectionTraversalCostModel().computeTraversalCost(
-                        traversedVertex, backPSE, this, traverseMode, options, (float) backSpeed,
-                        (float) speed);
-            } else {
-                // In case this is a temporary edge not connected to an IntersectionVertex
-                LOG.debug("Not computing turn cost for edge {}", this);
-                realTurnCost = 0;
-            }
-
-            if (options.preferBenches && (getBenchCount() > 0)) {
-                LOG.info("Found edge {}: {} has a bench!  Changing its turn cost.", getName(), getId());
-                realTurnCost *= 0.001;
-            }
-
-            if (options.preferToilets && (getToiletCount() > 0)) {
-                LOG.info("Found edge {}: {} has a toilet!  Changing its turn cost.", getName(), getId());
-                realTurnCost *= 0.001;
-            }
-
-            if (!traverseMode.isDriving()) {
-                s1.incrementWalkDistance(realTurnCost / 100);  // just a tie-breaker
-            }
-
-            long turnTime = (long) Math.ceil(realTurnCost);
-            time += turnTime;
-            weight += options.turnReluctance * realTurnCost;
-        }
-
-
-        if (walkingBike || TraverseMode.BICYCLE.equals(traverseMode)) {
-            if (!(backWalkingBike || TraverseMode.BICYCLE.equals(backMode))) {
-                s1.incrementTimeInSeconds(options.bikeSwitchTime);
-                s1.incrementWeight(options.bikeSwitchCost);
-            }
-        }
-
-        if (!traverseMode.isDriving()) {
-            s1.incrementWalkDistance(getDistance());
-        }
-
-        // accumulate feature counts
-        s1.incrementBenchCount(getBenchCount());
-        s1.incrementToiletCount(getToiletCount());
-
-        /* On the pre-kiss/pre-park leg, limit both walking and driving, either soft or hard. */
-        int roundedTime = (int) Math.ceil(time);
-        if (options.kissAndRide || options.parkAndRide) {
-            if (options.arriveBy) {
-                if (!s0.isCarParked()) s1.incrementPreTransitTime(roundedTime);
-            } else {
-                if (!s0.isEverBoarded()) s1.incrementPreTransitTime(roundedTime);
-            }
-            if (s1.isMaxPreTransitTimeExceeded(options)) {
-                if (options.softPreTransitLimiting) {
-                    weight += calculateOverageWeight(s0.getPreTransitTime(), s1.getPreTransitTime(),
-                            options.maxPreTransitTime, options.preTransitPenalty,
-                                    options.preTransitOverageRate);
-                } else return null;
-            }
-        }
-
-        /* Apply a strategy for avoiding walking too far, either soft (weight increases) or hard limiting (pruning). */
-        if (s1.weHaveWalkedTooFar(options)) {
-
-            // if we're using a soft walk-limit
-            if( options.softWalkLimiting ){
-                // just slap a penalty for the overage onto s1
-                weight += calculateOverageWeight(s0.getWalkDistance(), s1.getWalkDistance(),
-                        options.getMaxWalkDistance(), options.softWalkPenalty,
-                                options.softWalkOverageRate);
-            } else {
-                // else, it's a hard limit; bail
-                LOG.debug("Too much walking. Bailing.");
-                return null;
-            }
-        }
-
-        s1.incrementTimeInSeconds(roundedTime);
-        s1.incrementWeight(weight);
-
-        return s1;
-    }
-
-    private double calculateOverageWeight(double firstValue, double secondValue, double maxValue,
-            double softPenalty, double overageRate) {
-        // apply penalty if we stepped over the limit on this traversal
-        boolean applyPenalty = false;
-        double overageValue;
-
-        if(firstValue <= maxValue && secondValue > maxValue){
-            applyPenalty = true;
-            overageValue = secondValue - maxValue;
-        } else {
-            overageValue = secondValue - firstValue;
-        }
-
-        // apply overage and add penalty if necessary
-        return (overageRate * overageValue) + (applyPenalty ? softPenalty : 0.0);
     }
 
     /**
@@ -619,6 +359,7 @@ public class StreetEdge extends Edge implements Cloneable {
             clone.osmId = this.osmId;
             clone.benchCount = this.benchCount;
             clone.toiletCount = this.toiletCount;
+            clone.extras = this.extras;
             return clone;
         } catch (CloneNotSupportedException e) {
             throw new RuntimeException(e);
